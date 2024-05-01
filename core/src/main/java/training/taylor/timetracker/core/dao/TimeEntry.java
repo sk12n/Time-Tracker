ad4abc83703327d3294616f6d40977ac4e8baeed
sk12n <168687111+sk12n@users.noreply.github.com>
2024-05-01 18:54:27 -0400

    Update TimeEntry.java

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -3,9 +3,7 @@ package training.taylor.timetracker.core.dao;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-/**
- * Created by Jason on 6/19/2015.
- */
+
 @Component
 @Scope("prototype")
 public class TimeEntry {
