ab248bb45e13e75a3a97cc3e65c2cd0486f5704b
sk12n <168687111+sk12n@users.noreply.github.com>
2024-05-01 18:55:25 -0400

    Update TrackerCoreConfigTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
@@ -11,9 +11,7 @@ import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
 
-/**
- * Created by Jason on 6/20/2015.
- */
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TrackerCoreConfig.class)
 public class TrackerCoreConfigTest {
